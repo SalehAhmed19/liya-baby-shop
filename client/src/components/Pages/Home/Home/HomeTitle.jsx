a42dcb465be7a02e6a2cf1ac17eb82cff5788e83
Rashidaakter1 <rashidaakterchadni@gmail.com>
2023-01-07 00:47:35 +0600

    adding recommended products

--- client/src/components/Pages/Home/Home/HomeTitle.jsx
+++ client/src/components/Pages/Home/Home/HomeTitle.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomeTitle = ({ title }) => {
-  return <h2 className="capitalize">{title}</h2>;
+  return <h2 className="capitalize py-8">{title}</h2>;
 };
 
 export default HomeTitle;
--- client/src/components/Pages/Home/RecomendsProduct/RecomendsProduct.jsx
+++ client/src/components/Pages/Home/RecomendsProduct/RecomendsProduct.jsx
@@ -57,23 +57,42 @@ const RecommendsProduct = [
 
 const RecomendsProduct = () => {
   return (
-    <div className="container">
+    <div className="container mx-auto ">
       <HomeTitle title="Amala Earth Recommends" />
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-2">
-        {RecommendsProduct.map((data) => (
-          <div className="card w-64 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-              <img src={data?.img} alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{data?.title}</h2>
-              <p>{data.brands}</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
+      <div className=" grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-2 ">
+        
+      </div>
+      <div>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          slidesPerGroup={3}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {RecommendsProduct.map((data) => (
+            <SwiperSlide>
+              <div className="card w-64 border-2 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                  <img src={data?.img} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">{data?.title}</h2>
+                  <p>{data.brands}</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
