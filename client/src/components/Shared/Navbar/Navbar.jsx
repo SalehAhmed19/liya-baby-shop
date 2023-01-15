27a5b115c0c2f600ba39c9792a30111a31e80ef3
unknown <rashidaakterchadni@gmail.com>
2023-01-15 21:21:35 +0600

    products added

--- client/src/components/Shared/Navbar/Navbar.jsx
+++ client/src/components/Shared/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { MdCancel } from "react-icons/md";
 import NavLinks from "./NavLinks";
 import { AiOutlineMenu } from "react-icons/ai";
 import logo from "../../../Assets/icon/Liya Baby Shop-3.png";
+import { BsCartCheck, BsHeart } from "react-icons/bs";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
@@ -23,21 +24,102 @@ const Navbar = () => {
             </Link>
           </li>
           <NavLinks />
+          {/* search bar */}
+          <div className="form-control">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered"
+              />
+              <button className="btn btn-outline">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="">
+            <ul className="flex justify-between text-[white] px-3">
+              <Link to="/login" className="lg:ml-8">
+                Login
+              </Link>
+              <Link to="/login" className="lg:ml-8">
+                <div className="indicator">
+                  <span className="indicator-item badge badge-secondary">
+                    8
+                  </span>
+                  <div className="grid   place-items-center">
+                    <BsHeart className="text-3xl" />
+                  </div>
+                </div>
+              </Link>
+              <Link to="/login" className="lg:ml-8">
+                <div className="indicator">
+                  <span className="indicator-item badge badge-secondary">
+                    8
+                  </span>
+                  <div className="grid   place-items-center">
+                    <BsCartCheck className="text-3xl" />
+                  </div>
+                </div>
+              </Link>
+            </ul>
+          </div>
         </ul>
         <div className="md:block hidden">{/* <Button /> */}</div>
         {/* Mobile nav */}
         <ul
           className={`
-        md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+        md:hidden bg-[white] fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
         duration-500 ${open ? "left-0" : "left-[-100%]"}
          z-20`}
         >
           <li>
-            <Link to="/" className="py-7 px-3 inline-block">
+            <Link to="/" className="py-7 px-3 inline-block text-[black]">
               Home
             </Link>
           </li>
           <NavLinks />
+          <div>
+            <ul className="  lg:text-[white] px-3">
+              <Link to="/login" className="ml-4">
+                Login/Sign Up
+              </Link>
+              <Link to="/login" className="ml-4">
+                <div className="indicator">
+                  <span className="indicator-item badge badge-secondary">
+                    8
+                  </span>
+                  <div className="grid   place-items-center">
+                    <BsCartCheck className="text-3xl" />
+                  </div>
+                </div>
+              </Link>
+              <Link to="/login" className="ml-4">
+                <div className="indicator">
+                  <span className="indicator-item badge badge-secondary">
+                    8
+                  </span>
+                  <div className="grid   place-items-center">
+                    <BsCartCheck className="text-3xl" />
+                  </div>
+                </div>
+              </Link>
+            </ul>
+          </div>
           <div className="py-5">{/* <Button /> */}</div>
         </ul>
       </div>
